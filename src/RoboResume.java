989cf5ec7363b1aad3e72b83a6145bac01e7cb9a
Betty Negatu <Betty Negatu>
2017-06-08 06:39:09 -0400

    first commit

--- src/RoboResume.java
+++ src/RoboResume.java
@@ -1,79 +1,33 @@
 import java.util.Scanner;
 public class RoboResume {
-
-	public static void main(String[] args) 
-	{
-			      Scanner sc = new Scanner(System.in);
-			      System.out.println("Please Enter Full Name :");
+	
+				Scanner sc = new Scanner(System.in);
+				void print(){
+			      
+			      System.out.println("Please Enter First, Last Name :");
 			      String name =  sc.nextLine();
 			      
+			      while(!name.matches("[a-zA-z]+([ '-][a-zA-Z]+)*"))
+			      {
+			    	  System.out.println("Please Retype name First, Last name ");
+			    	  name =  sc.nextLine();
+			      }
+			    
 			      System.out.println("Please Enter Email Address :");
 			      String email =  sc.nextLine();
 			      
-			      System.out.println("Number of  Education Achivement :");
-			      int numOfAchivement = sc.nextInt();
-			     
-			      
-			      while(numOfAchivement < 1 || numOfAchivement>10) 
+			      while(!email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"))
 			      {
-			    	  System.out.println("Please Enter Number of  Education Achivement Again:");  
-			    	  numOfAchivement = sc.nextInt();
-			      }//while end 
-
-			      	     
-		  		String edu[] = new String[numOfAchivement];
-		  
-		  		 for (int i = 0; i < edu.length; i++)
-		  		{
-		  			 System.out.println("Enter Education Achivement");
-		  			 
-		  		  edu[i] = sc.next();
-		  		  sc.nextLine();
-		  		}
-			      
-		  		 
-			     System.out.println("Number of Work Experiences:");
-			      int numOfexperiences = sc.nextInt();
-			      
-			      while (numOfexperiences < 0 || numOfexperiences>10)
-			      {
-			    	  System.out.println("Please Enter Number of Work Experiences Again:");  
-			    	  numOfexperiences = sc.nextInt();
-			      }
-			      	String exp[] = new String[numOfexperiences];
-		  			
-		  			for (int i = 0; i < exp.length; i++)
-		  		{
-		  				System.out.println("Enter Work Experiences:");
-		  		   exp[i] = sc.next();
-		  		   sc.nextLine();
-		  		}	
-		  		
+			    	  System.out.println("Please Retype Email Address ");
+			    	  email =  sc.nextLine();
+			      }}}
 			      
 			      
-			      System.out.println("Number of  Skills with Rating :");
-			      int numOfSkills = sc.nextInt();
-			      while(numOfSkills < 1 || numOfSkills>20) 
-			      {
-			    	  System.out.println("Please Enter Number of  Skills with Rating Again:");  
-			    	  numOfSkills = sc.nextInt();
-			      }
-			       		
-			    		
-				    	String skill[] = new String[numOfSkills];
-				   		
-				   		
-				   		 for (int i = 0; i < skill.length; i++)
-				   		{
-				   			System.out.println("Enter Skills with Rating:");
-				   			skill[i] = sc.next();
-				   			sc.nextLine();
-				   			
-				   		}
+		
 			    		   
 				   		 
 				   		 // Displaying the information 
-				   		System.out.println( "\n"+"\n"+"Name:"+name);
+				   		/*System.out.println( "\n"+"\n"+"Name:"+name);
 				  		System.out.println("Email:"+ email+"\n");
 				   		 
 			    		System.out.println("Education Achivement:");
@@ -91,10 +45,11 @@ public class RoboResume {
 			    		for (int i = 0; i < skill.length; i++) 
 			    		{
 			    		 System.out.print(skill[i] + "\n"+"\n");
-			    		}
+			    		}*/
 			      
-			   }
-			}
+	
+
+			
 				
 
 			
---
+++ src/RoboResumeApp.java
@@ -0,0 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RoboResumeApp {
+
+	public static void main(String[] args) {
+		
+		RoboResume ro = new RoboResume();
+		Experience ex = new Experience();
+		ro.print();
+		ex.experience();
+		
+
+	}
+
+}
